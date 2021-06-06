b0fa84a704857d98293c3560adbab72d17d49144
Gustavo Urdaneta <seniortavo@gmail.com>
2021-06-06 17:53:02 -0300

    iniciacion del bot

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, CommandHandler
 
 def start(update, context):
-    updater.message.reply_text('Bienvenido al bot')
+    update.message.reply_text('Bienvenido al bot')
 if __name__ == '__main__':
 
     updater= Updater(token='1817000727:AAEo2uWuWutI9xKpZx_FCB7eX5gPtKlHfa0', use_context=True)
