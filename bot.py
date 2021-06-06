427eed8ffcbb9296b1454e05470f52dbbc3a05e7
Gustavo Urdaneta <seniortavo@gmail.com>
2021-06-06 17:47:30 -0300

    iniciacion del bot

---
+++ bot.py
@@ -0,0 +1,15 @@
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context)
+    updater.message.reply_text('Bienvenido al bot')
+if __name__ == '__main__':
+
+    updater= Updater(token='1817000727:AAEo2uWuWutI9xKpZx_FCB7eX5gPtKlHfa0', use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start',start))
+
+
+    # add handler
+
+    updater.start_polling()
+    updater.idle()
